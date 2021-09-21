55e7d4bc0f9bcdb17c5afa447b916c8139955ded
SerjLafleur <60075400+SerjLafleur@users.noreply.github.com>
2021-09-21 12:31:25 +0200

    Implementation of google maps in route details

--- src/components/DetailsRoute.js
+++ src/components/DetailsRoute.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
+import { Card, CardContent } from '@mui/material';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import { Link } from 'react-router-dom';
+import Map from '../Map'
 const DetailsRoute = () => {
+
+    const star = { lat: 37.4148533, lng: -4.5140462 }
+    const end = { lat: 37.4067561, lng: -4.4199274 }
 
     const classes = useStyles()
     return (
@@ -13,6 +18,22 @@ const DetailsRoute = () => {
                     <Link style={{ textDecoration: 'none', color: '#000' }} to='/list-routes'>Volver</Link>
                 </div>
                 <Typography variant="h3" color="initial">Listado de Rutas</Typography>
+            </div>
+            <div className={classes.map}>
+                <Map />
+            </div>
+            <div className={classes.card}>
+                <Card style={{ margin: '10px' }} sx={{ minWidth: 300 }}>
+                    <CardContent>
+                        <Typography className={classes.direction} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Desde: Dirección 1
+                        </Typography>
+                        <Typography className={classes.direction} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Hasta: Dirección 2
+                            <span>Fecha: 18/10/2019</span>
+                        </Typography>
+                    </CardContent>
+                </Card>
             </div>
         </div>
     );
@@ -30,6 +51,24 @@ const useStyles = makeStyles((theme) => ({
     back: {
         display: 'flex',
         alignItems: 'center',
+    },
+    map: {
+        overflow: 'hidden',
+        paddingButtom: '400px',
+        position: 'relative',
+        height: '250px',
+
+    },
+    card: {
+        marginTop: theme.spacing(5),
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    direction: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: theme.spacing(2)
     },
 }))
 
